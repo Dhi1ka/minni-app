04d4b006e45d966ec34d1127527170203b4b0d3f
dhi1ka <daw.knight04@gmail.com>
2022-03-11 15:28:12 +0700

    add features and add navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
 import { getPosts } from "./actions/posts";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   const [currentId, setCurrentId] = React.useState(null);
@@ -18,12 +19,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Minni
-        </Typography>
-        {/* <img src="" alt="" /> */}
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid
