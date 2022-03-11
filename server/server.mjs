49474b4e605f757730c7a1ec234c28dea2a64d35
dhi1ka <daw.knight04@gmail.com>
2022-03-11 09:59:13 +0700

    deploying

--- server/server.mjs
+++ server/server.mjs
@@ -21,11 +21,13 @@ app.use("/", (req, res) => {
   });
 });
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = +process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server listening on http://localhost:${PORT}`),
