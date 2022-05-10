6e367a7123fe32030a6707e5dda57dc7884da740
dhi1ka <daw.knight04@gmail.com>
2022-05-10 13:44:57 +0700

    api get posts pagination

--- server/controllers/PostController.mjs
+++ server/controllers/PostController.mjs
@@ -2,10 +2,23 @@ import mongoose from "mongoose";
 import Post from "../models/Post.mjs";
 
 export const getPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
+  const { page } = req.query;
 
-    res.status(200).json(posts);
+  try {
+    const LIMIT = 8;
+    const startIndex = (+page - 1) * LIMIT;
+    const total = await Post.countDocuments({});
+
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.status(200).json({
+      data: posts,
+      currentPage: +page,
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
