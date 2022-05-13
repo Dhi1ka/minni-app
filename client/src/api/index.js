c23bffe6eb45f4c82b1279ee8700ef9c9842638f
dhi1ka <daw.knight04@gmail.com>
2022-05-13 22:41:40 +0700

    api url test

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://minni-test.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://api-minni-test.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
   const profile = localStorage.getItem("profile");
